f8e43e0b65e71384258a588a8506ca631f734b17
lyndon <liuqiang89757@live.com>
2019-09-18 10:29:24 +0800

    Update common.js

--- common.js
+++ common.js
@@ -8,3 +8,82 @@ String.prototype.format = function() {
   }
   return s
 }
+
+export function parseTime(time, cFormat) {
+  if (arguments.length === 0) {
+    return null
+  }
+  const format = cFormat || '{y}-{m}-{d} {h}:{i}:{s}'
+  let date
+  if (typeof time === 'object') {
+    date = time
+  } else {
+    if ((typeof time === 'string') && (/^[0-9]+$/.test(time))) {
+      time = parseInt(time)
+    }
+    if ((typeof time === 'number') && (time.toString().length === 10)) {
+      time = time * 1000
+    }
+    date = new Date(time)
+  }
+  if (!(date instanceof Date)) return date
+  const formatObj = {
+    y: date.getFullYear(),
+    m: date.getMonth() + 1,
+    d: date.getDate(),
+    h: date.getHours(),
+    i: date.getMinutes(),
+    s: date.getSeconds(),
+    a: date.getDay()
+  }
+  const time_str = format.replace(/{(y|m|d|h|i|s|a)+}/g, (result, key) => {
+    let value = formatObj[key]
+    // Note: getDay() returns 0 on Sunday
+    if (key === 'a') { return ['日', '一', '二', '三', '四', '五', '六'][value ] }
+    if (result.length > 0 && value < 10) {
+      value = '0' + value
+    }
+    return value || 0
+  })
+  return time_str
+}
+
+export default {
+  install(Vue) {
+    /* 格式化Json */
+    Vue.prototype.$formatJson = (filterVal, jsonData) => {
+      return jsonData.map(v => filterVal.map(j => {
+        if (j === 'timestamp') {
+          return parseTime(v[j])
+        } else {
+          return v[j]
+        }
+      }))
+    }
+    
+    /* 复制 */
+    copyValue = (txt, showToast = false, copyEmpty = true) => {
+      if (!copyEmpty && !!!txt)
+        return
+
+      var textArea = document.createElement("textarea")
+      textArea.value = txt
+      document.body.appendChild(textArea)
+      textArea.select()
+      document.execCommand('copy')
+      document.body.removeChild(textArea)
+
+      if (showToast) {
+        Ext.toast({ html: '复制成功：【{0}】'.format(txt), autoCloseDelay: 800, slideInDuration: 100 }) // Ext写法
+      }
+    }
+
+    /* 获取Guid */
+    getGuid = () => {
+      return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+          var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8)
+          return v.toString(16)
+      })
+    }
+  }
+}
